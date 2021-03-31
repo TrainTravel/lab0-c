e422f577887b83053191f172f0f3bf282db6bf84
traintravel <train981316@gmail.com>
2021-03-31 05:08:55 +0000

    1.Finished q_reverse 2.Debug q_free

--- queue.c
+++ queue.c
@@ -5,6 +5,10 @@
 #include "harness.h"
 #include "queue.h"
 
+#ifndef strlcpy
+#define strlcpy(dst, src, sz) snprintf((dst), (sz), "%s", (src))
+#endif
+
 /*
  * Create empty queue.
  * Return NULL if could not allocate space.
@@ -14,6 +18,8 @@ queue_t *q_new()
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
@@ -22,16 +28,34 @@ void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
-    list_ele_t *head = q->head;
-    while (head) {
+    // if(!q)
+    //    return;
+    while (q->head) {
         list_ele_t *tmp;
-        tmp = head;
-        head = head->next;
+        // printf("current size: %d\n", q_size(q));
+        tmp = q->head;
+        q->head = q->head->next;
         free(tmp->value);
         free(tmp);
+        q->size -= 1;
     }
     free(q);
+    return;
 }
+// void q_free(queue_t *q)
+//{
+//    /* TODO: How about freeing the list elements and the strings? */
+//    /* Free queue structure */
+//    list_ele_t *head = q->head;
+//    while (head) {
+//        list_ele_t *tmp;
+//        tmp = head;
+//        head = head->next;
+//        free(tmp->value);
+//        free(tmp);
+//    }
+//    free(q);
+//}
 
 /*
  * Attempt to insert element at head of queue.
@@ -42,11 +66,10 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
     /* TODO: What should you do if the q is NULL? */
     if (!q)
         return false;
-    newh = malloc(sizeof(list_ele_t));
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     char *dest = malloc(strlen(s) + 1);
@@ -54,11 +77,13 @@ bool q_insert_head(queue_t *q, char *s)
         printf("Couldn't allocate space.");
         return false;
     }
-    strlcpy(dest, s);
+    strlcpy(dest, s, strlen(s) + 1);
     newh->value = dest;
     newh->next = q->head;
     q->head = newh;
     q->size += 1;
+    if (q->size == 1)
+        q->tail = newh;
     return true;
 }
 
@@ -74,7 +99,27 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
+    char *dest = malloc(strlen(s) + 1);
+
+    if (!q)
+        return false;
+    if (!dest || !newh) {
+        printf("Couldn't allocate space.");
+        return false;
+    }
+    strlcpy(dest, s, strlen(s) + 1);
+    newh->value = dest;
+    newh->next = NULL;
+
+    if (q->tail) {
+        // printf("q->tail isn't NULL!\n");
+        q->tail->next = newh;
+        q->tail = newh;
+    } else
+        q->tail = q->head = newh;
+    q->size += 1;
+    return true;
 }
 
 /*
@@ -114,8 +159,21 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    // Step1: First, assign the head node as the tail node.
+    // Step2: At each step, assign this (would-be) tail node's next node to be
+    // the head. Step3: Repeat Step2 until the tail becomes exactly the tail
+    // node(next node is NULL).
+    if (!q || !q->head)
+        return;
+    list_ele_t *tmp;
+    q->tail = q->head;
+    while (q->tail->next) {
+        tmp = q->tail->next;
+        q->tail->next = tmp->next;
+        tmp->next = q->head;
+        q->head = tmp;
+    }
+    return;
 }
 
 /*
